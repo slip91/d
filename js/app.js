9f4fa5998401c02c069a3f1b69bf2bf1c8eab728
user <slip912@gmail.com>
2025-03-08 04:42:48 +0400

    маркеры на карте + фиксы

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  // initialize Leaflet
-  var map = L.map('map').setView({lon: 0, lat: 0}, 2);
+  // Инициализация карты
+  var map = L.map('map').setView([51.958, 9.141], 13);
 
-// add the OpenStreetMap tiles
+  // Добавляем слой OpenStreetMap
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    // attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
   }).addTo(map);
 
-// show the scale bar on the lower left corner
-  L.control.scale().addTo(map);
+  const customIcon = L.divIcon({
+    className: 'custom-marker',
+    html: `
+      <div class="marker-container">
+        <img src="/img/storePin.svg" alt="Marker" class="marker-icon" />
+        <div class="info-box">
+          <h3 style="font-size: 20px; font-weight: 500; color: black">Laboratory</h3>
+          <p style="font-weight: 400; font-size: 15px; color: #818181 ">Closes 11PM</p>
+        </div>
+      </div>
+    `,
+    iconSize: [170, 69], // Размер иконки (ширина, высота)
+  });
 
-// show a marker on the map
-  L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(map);
+  // Создаем маркер с кастомной иконкой
+  const marker = L.marker([51.958, 9.141], {
+    icon: customIcon,
+  }).addTo(map);
 });
 
 module.exports = {
