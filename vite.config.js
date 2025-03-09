5733211bcf429477efa51c159d7c8725ccf82a2a
user <slip912@gmail.com>
2025-03-09 22:08:11 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,16 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  // base: '/dist',
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       main: './index.html',
-  //       page2: './index3.html',
-  //       labList: './lab_result.html',
-  //       profile: './profile.html',
-  //       youPartners: './your_partners.html',
-  //     }
-  //   }
-  // }
+  base: '/dist',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        page2: './index3.html',
+        labList: './lab_result.html',
+        profile: './profile.html',
+        youPartners: './your_partners.html',
+      }
+    }
+  }
 });
