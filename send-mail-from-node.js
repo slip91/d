1e29a36a4c7f277c5fcc40c906356382fd5ae271
user <slip912@gmail.com>
2025-03-23 05:36:50 +0400

    for history

--- send-mail-from-node.js
+++ send-mail-from-node.js
@@ -23,15 +23,40 @@ const message = {
   text: "Text if the HTML dont load2",
   html: content,
   attachments: [
-    // {
-    //   filename: 'logo_pink_white.svg',
-    //   path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\logo_pink_white.svg',
-    //   cid: 'logo_pink_white' //same cid value as in the html img src
-    // },
+    {
+      filename: 'logo_pink_white.png',
+      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\logo_pink_white.png',
+      cid: 'logo_pink_white' //same cid value as in the html img src
+    },
     {
       filename: 'top.png',
       path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\top.png',
       cid: 'top' //same cid value as in the html img src
+    },
+    {
+      filename: 'top_short.png',
+      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\top_short.png',
+      cid: 'top_short2' //same cid value as in the html img src
+    },
+    {
+      filename: 'phone.png',
+      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\phone.png',
+      cid: 'phone' //same cid value as in the html img src
+    },
+    {
+      filename: 'notification_container_email.png',
+      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\notification_container_email.png',
+      cid: 'notification_container_email' //same cid value as in the html img src
+    },
+    {
+      filename: 'rotated_background.png',
+      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\rotated_background.png',
+      cid: 'rotated_background' //same cid value as in the html img src
+    },
+    {
+      filename: 'noti_block.png',
+      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\noti_block.png',
+      cid: 'noti_block' //same cid value as in the html img src
     }
   ]
 };
