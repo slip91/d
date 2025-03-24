6f3325d61fcffbb4184476f8e723274e277a8d33
user <slip912@gmail.com>
2025-03-25 01:21:33 +0400

    for history

--- send-mail-from-node.js
+++ send-mail-from-node.js
@@ -1,6 +1,7 @@
 "use strict";
 const nodemailer = require("nodemailer");
 const fs = require("fs");
+const {join} = require("node:path");
 // HTML контент письма
 const content = fs.readFileSync('email.html','utf8');
 
@@ -24,44 +25,34 @@ const message = {
   html: content,
   attachments: [
     {
-      filename: 'logo_pink_white.png',
-      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\logo_pink_white.png',
-      cid: 'logo_pink_white' //same cid value as in the html img src
+      filename: 'logopinkwhite.png',
+      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\logopinkwhite.png',
+      cid: 'logopinkwhite', //same cid value as in the html img src
+      contentType: "image/png"
     },
     {
-      filename: 'top.png',
-      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\top.png',
-      cid: 'top' //same cid value as in the html img src
-    },
-    {
-      filename: 'top_short.png',
-      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\top_short.png',
-      cid: 'top_short3' //same cid value as in the html img src
+      filename: 'topshort.png',
+      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\topshort.png',
+      cid: 'topshort3',//same cid value as in the html img src
+      contentType: "image/png"
     },
     {
       filename: 'phone.png',
       path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\phone.png',
-      cid: 'phone' //same cid value as in the html img src
+      cid: 'phone', //same cid value as in the html img src
+      contentType: "image/png"
     },
     {
-      filename: 'notification_container_email.png',
-      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\notification_container_email.png',
-      cid: 'notification_container_email' //same cid value as in the html img src
-    },
-    {
-      filename: 'rotated_background.png',
-      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\rotated_background.png',
-      cid: 'rotated_background' //same cid value as in the html img src
-    },
-    {
-      filename: 'noti_block.png',
-      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\noti_block.png',
-      cid: 'noti_block2' //same cid value as in the html img src
+      filename: 'notiblock.png',
+      path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\notiblock.png',
+      cid: 'notiblock', //same cid value as in the html img src
+      contentType: "image/png"
     },
     {
       filename: 'unilab.png',
       path: 'C:\\Users\\user\\WebstormProjects\\denis\\img\\unilab.png',
-      cid: 'unilab' //same cid value as in the html img src
+      cid: 'unilab', //same cid value as in the html img src
+      contentType: "image/png"
     }
   ]
 };
